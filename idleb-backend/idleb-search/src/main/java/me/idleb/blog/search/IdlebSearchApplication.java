43860d12d8a306ad3fe4e3773cc4a86df753aafc
Jin.X <>
2019-06-07 19:03:18 +0800

    确定服务端功能模块及目录

---
+++ idleb-backend/idleb-search/src/main/java/me/idleb/blog/search/IdlebSearchApplication.java
@@ -0,0 +1,13 @@
+package me.idleb.blog.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdlebSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdlebSearchApplication.class, args);
+    }
+
+}
---
+++ idleb-backend/src/main/java/me/idleb/blog/IdlebBackendApplication.java
@@ -0,0 +1,13 @@
+package me.idleb.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdlebBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdlebBackendApplication.class, args);
+	}
+
+}
